849c1218bff4adf42529d97eaefae015c5859730
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-03 18:00:22 +0100

    making some little adjustment

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -82,7 +82,7 @@ export const Hero: React.FunctionComponent  = () => {
 
 
         
-          <div className=''>
+          <div className=' text-center'>
             <p className="w-[17.125rem] h-[3.125rem] md:w-[46.375rem] md:h-[4rem] mx-auto text-[0.875rem] mt-[10px] md:mt-[16px] text-[#ccc] md:text-[1.5rem] sub-intro leading-[120%]"> Frontend Magic, From your vision to reality, witness the  power of turning concepts into captivating  user-centric design.</p>
           </div>
 
