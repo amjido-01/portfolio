7bc3c0ad4c3459b3eb6789ef20d8bb621e6bf52a
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-03 17:57:31 +0100

    making some little adjustment

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -7,8 +7,8 @@ import smUnion from "@/assets/smUnion.svg";
 import lgUnion from "@/assets/lgUnion.svg"
 // import { MdStars } from './MdStarts';
 // import { Start } from './Star';
-import {Test } from "./Test"
 import { HamburgerMenu } from './HamburgerMenu';
+import {Test } from "./Test"
 // import Star from "../../assets/Star.svg"
 // import Typed from "typed.js";
 // import { useEffect } from 'react';
@@ -35,18 +35,15 @@ export const Hero: React.FunctionComponent  = () => {
     className="hero test text-white pt-20 relative overflow-hidden">
       <HamburgerMenu />
       {/* <img src={union} className='absolute -top-[16rem] -right-24' alt="" /> */}
-      <div>
-        <img src={smUnion} alt="" className='absolute top-0 right-0 block sm:hidden'/>
+   <div>
+       <img src={smUnion} alt="" className='absolute top-0 right-0 block sm:hidden'/>
    
       <img src={lgUnion} className='absolute hidden sm:block -top-[4rem] -right-[14rem] md:-top-8 md:-right-[10rem] lg:right-0' />
       <div className=' md:w-[86%] mt-[60px] md:mt-[150px] mx-auto'>
         <div className="text-center mx-auto text-[#fff]">
-          <div className='relative w-[19rem] md:w-[46.375rem] border2 md:h-[7.8125rem] mx-auto'>
-
-            {/* <img src={Star} alt="" className='border-2'/> */}
-            {/* <Start className='absolute -left-4 -top-7 border-2'/> */}
+          <div className='relative w-[20rem] md:w-[46.375rem] md:h-[7.8125rem] mx-auto'>
             
-            <div className='border2 w-fit'>
+            <div className='w-fit'>
               <div className=''>
                 <Test animationDuration={8} className='absolute -top-[2rem] -left-12 md:-left-14 md:-top-[2.5rem] mx-auto w-[2rem] h-[2rem] md:w-[2.5rem] md:h-[2.5rem]'/>
 
@@ -54,18 +51,42 @@ export const Hero: React.FunctionComponent  = () => {
 
               <Test animationDuration={4} className='absolute -left-8 -top-[2rem] md:-top-[2.5rem] mx-auto w-[1.5rem] h-[1.5rem]'/>
               </div>
+
                <Test animationDuration={7} className='absolute left-0 -top-6 md:-top-8 md:left-[1rem] w-[1.5rem] h-[1.5rem] md:w-[2rem] md:h-[2rem]'/>
                 <Test animationDuration={4}  className='absolute -top-[2rem] -left-[1.4rem] md:-left-[1rem] md:-top-[2rem] w-[2.2rem] h-[2.2rem] md:w-[3rem] md:h-[3rem]'/>
               <Test animationDuration={10}  className='absolute borde -top-[1rem] -left-7 md:-top-5 md:-left-[1rem] w-[3rem] h-[3rem] md:w-[5rem] md:h-[5rem]'/>
-              {/* <Test className='absolute'/>   */}
             </div>
-            <h1 className=' intro text-[1.5rem] md:text-[3.25rem] leading-[120%] uppercase'>Transforming Ideas into Digital <span 
-          id="target"
-          className='gradient-text'>Reality</span></h1>
+
+              <h1 className=' intro text-[1.5rem] md:text-[3.25rem] leading-[120%] uppercase'>Transforming Ideas</h1>
+
+            <div className='text-center wraps h-[30px] md:h-[60px] intro text-[1.5rem] md:text-[3.25rem] leading-[120%] uppercase'>
+              <h1 className=''>into Digital</h1>
+              <div className='word overflow-hidden'>
+                <span
+            className='gradient-text'>Reality
+            </span>
+                <span 
+              id="target"
+            className='gradient-text'>Experience
+            </span>
+                <span 
+              id="target"
+            className='gradient-text'>Solution
+            </span>
+              </div>
+            </div>
+
           </div>
 
-          <p className="w-[17.125rem] h-[3.125rem] md:w-[46.375rem] md:h-[4rem] mx-auto text-[0.875rem] mt-[10px] md:mt-[16px] text-[#ccc] md:text-[1.5rem] sub-intro leading-[120%]"> Frontend Magic, From your vision to reality, witness the  power of turning concepts into captivating  user-centric design.</p>
         </div>
+
+
+        
+          <div className=''>
+            <p className="w-[17.125rem] h-[3.125rem] md:w-[46.375rem] md:h-[4rem] mx-auto text-[0.875rem] mt-[10px] md:mt-[16px] text-[#ccc] md:text-[1.5rem] sub-intro leading-[120%]"> Frontend Magic, From your vision to reality, witness the  power of turning concepts into captivating  user-centric design.</p>
+          </div>
+
+
         <div className="btn flex justify-center items-center mt-[4rem]">
           <Button size="lg" className='text-[#fff] py-[0.75rem] rounded-[0.25rem] px-[2.5rem] md:w-[12.5rem] md:h-[3.125rem] md:py-[1rem] text-[0.76019rem] md:text-[1rem] capitalize leading-[120%] md:px-[4rem]'>Projects</Button>
         </div>
@@ -74,7 +95,7 @@ export const Hero: React.FunctionComponent  = () => {
           <LgDownArrow className='hidden  md:block w-[4rem] h-[4rem]'/>
         </div>
       </div>
-      </div>
+   </div>
     </div>
   )
 }
