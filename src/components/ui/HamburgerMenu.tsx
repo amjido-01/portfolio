77ca143a43882888cd7f62c80a02f3d8da0052bb
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-04 08:38:39 +0100

    hambugar menu

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -38,7 +38,7 @@ export const HamburgerMenu:React.FunctionComponent = () => {
           } `}
         >
           <ul
-            className="flex border-2 flex-col justify-end md:p-4 w-[70%] md:w-[60%] mt-[400px] ml-[80px] md:mt-[90px] md:ml-[100px] md:mb-[100px"
+            className="flex border-2 flex-col justify-end md:p-4 w-[70%] md:w-[60%] mt-[250px] sm:mt-[400px] ml-[80px] md:mt-[90px] md:ml-[100px] md:mb-[100px"
           >
             <li className="">
              <Links to="/" handleRoute={(e) => {
