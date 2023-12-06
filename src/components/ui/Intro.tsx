df55caeaf730e1d8526d9f3f3e2ecbd1248eb447
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-06 23:38:42 +0100

    hambugar menu

--- src/components/ui/Intro.tsx
+++ src/components/ui/Intro.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const Intro: React.FunctionComponent = () => {
   return (
     <div className="mx-auto text-[#fff]">
-          <div className='relative w-[21rem] border2 md:w-[48.625rem] bord md:h-[7.8125rem] mx-auto uppercase'>
+          <div className='relative w-[24rem] border2 md:w-[48.625rem] bord md:h-[7.8125rem] mx-auto uppercase'>
             
-              <h1 className='intro borde text-[2rem] md:text-[3.25rem] leading-[120%]  textcenter md:text-start'>Transforming Ideas <span className='md:hidden'>into Digital</span></h1>
+              <h1 className='intro borde text-[2rem] md:text-[3.25rem] leading-[120%] textcenter md:text-start'>Transforming Ideas <span className='md:hidden intro'>into Digital</span></h1>
 
             <div className='spin-text-container flex justify-start md:justify-center md:w-[47.5rem] border2 h-[30px] md:h-[60px] intro text-[2rem] md:text-[3.25rem] leading-[120%]'>
 
