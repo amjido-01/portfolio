fa8a0f9595dd6a26dae60f42dff3eee863375f46
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-06 23:27:32 +0100

    hambugar menu

--- src/components/ui/Intro.tsx
+++ src/components/ui/Intro.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const Intro: React.FunctionComponent = () => {
   return (
     <div className="mx-auto text-[#fff]">
-          <div className='relative w-[20rem] md:w-[46.625rem] bord md:h-[7.8125rem] mx-auto uppercase'>
+          <div className='relative w-[21rem] border2 md:w-[48.625rem] bord md:h-[7.8125rem] mx-auto uppercase'>
             
-              <h1 className='intro borde text-[2rem] md:text-[3.25rem] leading-[120%]  textcenter md:text-start'>Transforming Ideas <span className='md:hidden'>nto Digital</span></h1>
+              <h1 className='intro borde text-[2rem] md:text-[3.25rem] leading-[120%]  textcenter md:text-start'>Transforming Ideas <span className='md:hidden'>into Digital</span></h1>
 
-            <div className='spin-text-container flex justify-start md:justify-center md:w-[48rem] border2 h-[30px] md:h-[60px] intro text-[2rem] md:text-[3.25rem] leading-[120%]'>
+            <div className='spin-text-container flex justify-start md:justify-center md:w-[47.5rem] border2 h-[30px] md:h-[60px] intro text-[2rem] md:text-[3.25rem] leading-[120%]'>
 
               <h1 className='intro textcenter md:text-start hidden md:block'>into Digital</h1>
               <div className='word text-start'>
