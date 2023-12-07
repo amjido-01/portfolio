0e24992b4f3abbb67d1f2afaa2afe60189522cbc
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-07 05:53:53 +0100

    hambugar menu

--- src/components/ui/SubIntro.tsx
+++ src/components/ui/SubIntro.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const SubIntro: React.FunctionComponent = () => {
   return (
-        <p className="w-[20.125rem] borde md:w-[48.375rem] md:h-[4rem] border-2 mx-auto text-[0.875rem] mt-[0.56rem] md:mt-[0.88rem] text-[#ccc] md:text-[1.5rem] sub-intro leading-[120%]"> Frontend Magic, From your vision to reality, witness the  power of turning concepts into captivating  user-centric design.</p>
+        <p className="w-[20.125rem] md:w-[48.375rem] md:h-[4rem] border2 mx-auto text-[0.875rem] mt-[0.56rem] md:mt-[0.88rem] text-[#ccc] md:text-[1.5rem] sub-intro leading-[120%]"> Frontend Magic, From your vision to reality, witness the  power of turning concepts into captivating  user-centric design.</p>
   )
 }
