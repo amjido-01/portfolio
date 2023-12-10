5e856d0f2c5630ca05b5db3cd8d4ea74e3701bb6
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-10 06:20:20 +0100

    my bio read more btn

--- src/components/ui/Me.tsx
+++ src/components/ui/Me.tsx
@@ -7,7 +7,7 @@ export const Me: React.FunctionComponent = () => {
   return (
     <section className='border2 h-[30re mt-[10rem] md:mt-[15rem] overflow-hidden'>
 
-        <div className='mx-auto w-[90%] lg:w-[85%] flex flex-col sm:flex-col gap-[5rem] lg:flex-row justify-center  lg:gap-[7.5rem] items-center border-2'>
+        <div className='mx-auto w-[90%] lg:w-[85%] flex flex-col sm:flex-col gap-[5rem] lg:flex-row justify-center  lg:gap-[7.5rem] items-center border2'>
 
         <div className='w-[95%] sm:w-[90%] md:w-[85%] lg:w-[30.00006rem]'>
           <img src={ala} className='mx-auto' alt="my pic" />
@@ -17,7 +17,7 @@ export const Me: React.FunctionComponent = () => {
             <div className="content capitalize leading-normal border2 pl-2 md:pl-0">
               <h1 className='montserrat border2 text-[#fff] text-[1.5rem] md:text-[2.25rem] style font-bold leading-normal capitalize'>Hi there, I’m Ala jido </h1>
 
-              <p className='border2 Poppins text-[#ccc] text-[0.875rem] md:text-[1.25rem] font-normal capitalize md:my-[1.88rem]'>Frontend Developer and Computer Science Student with over 2 years of experience. Specializing in ReactJS, I'm committed to crafting cutting-edge, user-centric interfaces, prioritizing accessibility. Let's collaborate to bring your vision to life – your vision, my expertise.</p>
+              <p className='border2 Poppins text-[#ccc] text-[0.875rem] md:text-[1.25rem] font-normal capitalize md:my-[1.88rem]'>Frontend Developer and Computer Science Student with over 2 years of experience. Specializing in ReactJS, I'm committed to crafting cutting-edge, user-centric interfaces, prioritizing accessibility. Let's collaborate to bring your vision to life – your goals, my expertise.</p>
             </div>
             
             <div className="footer border2 flex mt-[1.875rem] gap-[1.875rem] w-full mx-auto md:mx-0 border2">
