1dab7595edff99cd867a4a79e6ec4f276f74d050
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-15 16:45:12 +0100

    working on the projects section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
 export const About: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(true)
   return (
     <div className="pt-20 border-2">
-    {/* <SkeletonTheme baseColor="#FF0000" highlightColor="#444">
-    <p className='border-2'>
-      <Skeleton count={3} height='20rem'/>
-    </p>
-  </SkeletonTheme> */}
-  
-  {loading ? <Skeleton
-  style={{display: 'block'}}
-    baseColor="#edebeb"
-    highlightColor="#d6d6d6"
-    count={4}
-    width='100%'
-    height={40}
-  /> : <p>hello</p>}
     </div>
   );
 };
