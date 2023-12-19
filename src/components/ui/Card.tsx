59b47af89783012c3aaed464e7b30021ba34e5e7
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-19 21:51:05 +0100

    rfactoring

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -20,37 +20,39 @@ export const Card: React.FunctionComponent<CardProps> = ({card}) => {
   return (
     <main
       key={card.id}
-      className="group relative rounded-[1.5rem] h-[450px] w-[450px] overflow-hidden bg-neutral-200"
-    >
+      className={` ${card.pic ? 'bg-[#0F172D]' : "bg-transparent"} group relative rounded-[1.5rem] w-[23.9375rem] h-[17.47263rem] md:h-[25rem] md:w-[34.25rem] overflow-hidden
+  `}>
       {/* Render the background image */}
       <div
-        style={{
-          background: `#0F172D`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
         className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
       ></div>
-      <div className="absolute flex h-full border-2 gap-[4.56rem]">
-        
+      {card.pic ?  <div className="absolute flex h-full border-2 gap-[4.56rem]">
        <div className="aside w-[18rem] h-full bullet">
-           <img src={card.pic} alt={card.title} className='absolute md:w-[6.28488rem] h-[6.28488rem] object-cover left-4 z-10'/>
+         <img src={card.pic} alt={card.alt} className='absolute md:w-[6.28488rem] h-[6.28488rem] object-cover left-4 z-10'/>
        </div>
        
        <div className="content p-[3.23rm] border-2">
           <div>
-           <h3 className="text-[#fff]">Muhammad Niimatullahi</h3>
+           <h3 className="text-[#fff]">{card.name}</h3>
             <p className="text-[#CCCCCC]">product designer</p>
           </div>
           
           <div className="body">
             <p>
-            “Lorem ipsum dolor sit amet consectetur. Fermentum convallis semper integer adipiscing adipiscing nibh eget. Posuere neque semper gravida massa cursus turpis vulputate. Mi maecenas.”
+            {card.content}
             </p>
           </div>
        </div>
        
-      </div>
+      </div>: <div className="absolute flex justify-center items-center w-full h-full">
+        <div className="">
+          <h1 className="montserrat text-[#fff] text-[1.5rem] md:text-[2.25rem] font-black leading-normal capitalize my-[1.38rem] md:my-[4rem]">
+          clients <br /> testimonials
+        </h1>
+        </div>
+      </div> }
+     
+      
     </main>
   );
 }
--- src/testimonyData.ts
+++ src/testimonyData.ts
@@ -2,11 +2,11 @@ import niima from '@/assets/niima.png'
 export const testimonyCards = [
   {
     id: 1,
-    title: "Title 1",
-    name: "Muhammad Niimatullahi",
-    pic: niima,
-    content: "“Lorem ipsum dolor sit amet consectetur. Fermentum convallis semper integer adipiscing adipiscing nibh eget. Posuere neque semper gravida massa cursus turpis vulputate. Mi maecenas.”",
-    alt: 'testimony'
+    title: "title 1",
+    // name: "Muhammad Niimatullahi",
+    // pic: niima,
+    // content: "“Lorem ipsum dolor sit amet consectetur. Fermentum convallis semper integer adipiscing adipiscing nibh eget. Posuere neque semper gravida massa cursus turpis vulputate. Mi maecenas.”",
+    // alt: 'testimony'
   },
   {
      id: 2,
