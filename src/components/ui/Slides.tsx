58d7da707c9b3addb3997445159cce368d344f56
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-22 16:50:18 +0100

    institutions

--- src/components/ui/Slides.tsx
+++ src/components/ui/Slides.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Organizations } from '@/components/ui/Organizations';
 import kust from '@/assets/kust.png';
+import Arrow from '@/assets/Arrow.svg';
+import ArrowSm from '@/assets/ArrowSm.svg';
 import gdsc from '@/assets/gdsc.png';
 import stem from '@/assets/stem.png';
 import bench from '@/assets/bench.png';
@@ -27,7 +29,11 @@ const logos2 = [
 export const Slides: React.FunctionComponent = () => {
   return (
     <div className='mt-[2.94rem] md:mt-[6.88rem] mb-24'>
-        <h1 className='text-[#fff] text-[1rem] font-black montserrat style capitalize text-center md:text-[1.5rem]'>institutes I've work with </h1>
+       <div>
+         <h1 className='text-[#fff] text-[1rem] font-black montserrat style capitalize text-center md:text-[1.5rem]'>institutes I've work with </h1>
+        <img src={Arrow} alt="" className='mx-auto hidden md:block'/>
+        <img src={ArrowSm} alt="" className='mx-auto block md:hidden'/>
+       </div>
        <div className='mt-[2.68rem] md:mt-[4.5rem] flex flex-col gap-[2.61rem] md:gap-[4rem]'>
          <Organizations direction='right' logos={logos}/>
       <Organizations direction='left' logos={logos2}/>
