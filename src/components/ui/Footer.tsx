96b650263eea63dbbbc6624fde88105a8ac367d7
alajido <96017433+Alajido@users.noreply.github.com>
2023-12-24 20:51:38 +0100

    contact me

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -5,57 +5,68 @@ import linkedin from '@/assets/linkedin.svg';
 import twitter from '@/assets/twitter.svg';
 import facebook from '@/assets/facebook.svg';
 import whatsapp from '@/assets/whatsapp.svg';
+import { Link } from 'react-router-dom';
 
 
 export const Footer: React.FunctionComponent = () => {
   return (
-<footer className="bg-[#0E162A] mt-[6.14rem] h-[82vh] border-2 border-red-500 dark:bg-gray-900 w-full">
+<footer className="bg-[#0E162A] mt-[6.14rem] h-[70vh] md:h-[82vh] w-full">
 
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 
-        <div className='flex border-2 justify-center items-center'>
+        <div className='flex justify-center items-center'>
 
-            <div className="flex flex-col gap-[4rem] border-2 border-yellow-500">
+            <div className="flex flex-col gap-[2.19rem] md:gap-[4rem]">
                 
-            <div className="w-full flex justify-center items-center border-2">
+            <div className="w-full flex justify-center items-center">
                 <img src={footerlogo} className='w-[10.125rem] h-[5.625rem]' alt="logo" />
             </div>
 
           <div>
-            <ul className='flex gap-[2.75rem]'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Blogs</li>
-                <li>Projects</li>
-                <li>Resume</li>
+            <ul className='flex text-[#fff] gap-[1.46rem] md:gap-[2.75rem] montserrat text-[0.875rem] md:text-[1.25rem] style font-bold leading-normal'>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/blogs'>Blogs</Link>
+                </li>
+                <li>
+                    <Link to='/projects'>Project</Link>
+                </li>
+                <li>
+                    <Link to='/resume'>Resume</Link>
+                </li>
             </ul>
           </div>
 
           <div>
-            <ul className='flex justify-center gap-[2.75rem]'>
+            <ul className='flex justify-center gap-[2rem] md:gap-[2.75rem]'>
                 <li>
                     <a href="https://github.com/sahil143">
-                        <img src={github} className='w-[2.5rem] h-[2.5rem]' alt="github" />
+                        <img src={github} className='w-[1.375rem] h-[1.375rem] :w-[2.5rem] :h-[2.5rem]' alt="github" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/sahil-shah-7019051a5>">
-                        <img src={twitter} className='w-[2.5rem] h-[2.5rem]' alt="linkedin" />
+                        <img src={twitter} className='w-[1.375rem] h-[1.375rem] :w-[2.5rem] :h-[2.5rem]' alt="linkedin" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/sahil-shah-7019051a5>">
-                        <img src={facebook} className='w-[2.5rem] h-[2.5rem]' alt="linkedin" />
+                        <img src={facebook} className='w-[1.375rem] h-[1.375rem] :w-[2.5rem] :h-[2.5rem]' alt="linkedin" />
                         </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/sahil-shah-7019051a5>">
-                        <img src={whatsapp} className='w-[2.5rem] h-[2.5rem]' alt="linkedin" />
+                        <img src={whatsapp} className='w-[1.375rem] h-[1.375rem] :w-[2.5rem] :h-[2.5rem]' alt="linkedin" />
                         </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/sahil-shah-7019051a5>">
-                        <img src={linkedin} className='w-[2.5rem] h-[2.5rem]' alt="linkedin" />
+                        <img src={linkedin} className='w-[1.375rem] h-[1.375rem] :w-[2.5rem] :h-[2.5rem]' alt="linkedin" />
                         </a>
                 </li>
             </ul>
@@ -64,8 +75,8 @@ export const Footer: React.FunctionComponent = () => {
         </div>
 
 
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <hr className="my-[4rem] md:my-[4rem] sm:mx-auto lg:my-8" />
+        <span className="block my-[4rem] md:my-[4rem] montserrat text-center style text-[0.625rem] md:text-[0.875rem] font-normal leading-normal capitalize  text-[#fff]">Copyright © 2023 All Rights Reserved alerntech</span>
     </div>
 </footer>
   )
