8ff8f2b5c9880e4bf2a2b2bd3e4d19d114cf973b
alajido <96017433+Alajido@users.noreply.github.com>
2024-01-01 14:37:06 +0100

    refactoring

--- src/blogCardData.ts
+++ src/blogCardData.ts
@@ -1,11 +1,11 @@
 import blogcard from '@/assets/blogcard.png'
-import test2 from '@/assets/test2.jpg';
-import test3 from '@/assets/test3.jpg';
-import test4 from '@/assets/test4.jpg';
+// import test2 from '@/assets/test2.jpg';
+// import test3 from '@/assets/test3.jpg';
+// import test4 from '@/assets/test4.jpg';
 import test1 from '@/assets/test1.jpg';
-import test5 from '@/assets/test5.jpg';
-import test6 from '@/assets/test6.jpg';
-import test7 from '@/assets/test7.jpg';
+// import test5 from '@/assets/test5.jpg';
+// import test6 from '@/assets/test6.jpg';
+// import test7 from '@/assets/test7.jpg';
 
 
 interface dataprops {
